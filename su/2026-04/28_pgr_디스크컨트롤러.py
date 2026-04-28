0a805f7f0b07c097d4ea817c2a093babc8bb3a2c
sujin <bellecode20@gmail.com>
2026-04-29 00:54:36 +0900

    fix: [260428][su] pgr 디스크 컨트롤러

--- su/2026-04/28_pgr_디스크컨트롤러.py
+++ su/2026-04/28_pgr_디스크컨트롤러.py
@@ -36,31 +36,32 @@ print(solution([[0, 3], [1, 9], [3, 5]])) # 결과: 8
 def solution2(jobs):
     answer = 0
     hq = []
-    # 요청 시각 기준으로 정렬
-    jobs.sort()
-    
     N = len(jobs)
+    
+    # 1. 원본 데이터 정렬 (요청 시간 기준)
+    jobs.sort()    
+    
     cur_time = 0
     i = 0
-    finished_count = 0
+    count = 0  # 처리 완료된 작업 수
     
-    while finished_count < N:
-        # 1. 현재 시간(cur_time) 이전에 요청된 모든 작업을 힙에 삽입
+    while count < N:
+        # 수정 포인트 1: 현재 시간(cur_time)까지 도착한 '모든' 작업을 힙에 삽입
         while i < N and jobs[i][0] <= cur_time:
-            # (소요시간, 요청시각) 순으로 push
-            heapq.heappush(hq, (jobs[i][1], jobs[i][0]))
+            start, duration = jobs[i]
+            heapq.heappush(hq, (duration, start)) # 소요시간 우선순위
             i += 1
-            
+        
+        # 수정 포인트 2: 처리할 작업이 있는 경우
         if hq:
-            # 2. 힙에서 가장 짧은 작업 꺼내서 처리
-            duration, start = heapq.heappop(hq)
-            cur_time += duration
-            answer += (cur_time - start)
-            finished_count += 1
+            d, s = heapq.heappop(hq)
+            cur_time += d
+            answer += (cur_time - s)
+            count += 1 # 작업 하나 완료
+            
+        # 수정 포인트 3: 처리할 작업이 없다면 다음 작업의 요청 시간으로 이동
         else:
-            # 3. 힙이 비어있다면, 다음 작업 요청 시각으로 시간 점프
-            if i < N:
-                cur_time = jobs[i][0]
+            cur_time = jobs[i][0]
                 
     return answer // N
 
