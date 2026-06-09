7adfdf8d951b50a58b840d41fd482236a1f869cd
Duck <dksejrgus2@naver.com>
2026-06-09 23:13:18 +0900

    [260610][duck]pgr택배상자

---
+++ 덕현/2026_06/10_pgr_택배상자.py
@@ -0,0 +1,34 @@
+'''
+프로그래머스 131704 택배상자
+
+입력:
+order
+
+구조:
+stack으로 했던거
+'''
+
+
+def solution(order):
+    answer = 0
+    stack = []
+
+    for i in range(1, len(order) + 1):
+
+        stack.append(i)
+
+        while stack:
+            now = stack[-1]
+
+            if now == order[answer]:
+                stack.pop()
+                answer += 1
+
+
+            else:
+                break
+
+            if answer == len(order):
+                break
+
+    return answer
