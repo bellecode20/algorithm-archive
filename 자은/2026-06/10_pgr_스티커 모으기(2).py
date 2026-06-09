2491982ccf9e356a607a8b7cbe39a0e04abe80d4
kwonjaeun <rnjswkdms123@naver.com>
2026-06-09 21:46:48 +0900

    [260610][자은] pgr 스티커 모으기(2)

--- 자은/2026-06/10_pgr_스티커 모으기(2).py
+++ 자은/2026-06/10_pgr_스티커 모으기(2).py
@@ -4,14 +4,14 @@ def solution(sticker):
     if len(sticker)==1:return sticker[0]
     if len(sticker)==2:return max(sticker)
     
-    #첫번째 값을 포함할때
+    #첫번째 값을 포함할 때
     dp1=[0]*len(sticker)
     dp1[0]=sticker[0]
     dp1[1]=sticker[0]
     for i in range(2,len(sticker)-1):
         dp1[i]=max(dp1[i-1],dp1[i-2]+sticker[i])
     
-    #첫번째 값을 포함하지 않을때
+    #첫번째 값을 포함하지 않을 때
     dp2=[0]*len(sticker)
     dp2[1]=sticker[1]
     dp2[2]=max(sticker[1],sticker[2])
