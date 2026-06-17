252d240f3a93b03ee0dc51e8f3b4d7c6565b892d
sujin <bellecode20@gmail.com>
2026-06-17 15:13:03 +0900

    [260617][su] sql 고양이와 개는 몇마리 있을까

---
+++ su/2026-06/17_sql_고양이와개는몇마리있을까.sql
@@ -0,0 +1,7 @@
+# 동물 보호소에 들어온 동물 중 고양이와 개가 각각 몇 마리인지 조회하는 SQL문 작성
+# 고양이를 개보다 먼저 조회하기
+SELECT ANIMAL_TYPE, COUNT(*) AS count
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE = "Cat" OR ANIMAL_TYPE = "Dog"
+GROUP BY ANIMAL_TYPE
+ORDER BY count
